7247a10ccd6e81b63fcec1a4baba5a9f79ee65f9
Harsha <harshavardhankond@gmail.com>
2025-03-06 14:54:37 +0530

    added

---
+++ Switch/Switch.js
@@ -0,0 +1,9 @@
+let Apple="Fruit";
+switch(Apple){
+    case "vegitable":
+        console.log("Apple is a vegitable");
+        break;
+        case "Fruit":
+        console.log("apple is a Fruit");
+        break;
+}

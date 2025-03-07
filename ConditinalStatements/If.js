deb0eb38ce5b5d2d7a951599d7c6f75a7301b85a
Harsha <harshavardhankond@gmail.com>
2025-03-07 14:42:54 +0530

    added the example file

--- ConditinalStatements/If.js
+++ ConditinalStatements/If.js
@@ -6,3 +6,16 @@ if(!(a==b)){
 if(a<b){
    console.log("bye")
 }
+
+
+let c=30;
+let d=40;
+if(c==30 && d==40 ){
+    console.log("done")
+}
+
+let e=50;
+let f=60;
+if(e==50 || f==60){
+    console.log("crt")
+}
---
+++ Loops/ForLoop.js
@@ -0,0 +1,27 @@
+let a=20;
+let b=30;
+if(a<b){
+    console.log("The largest number is"+b)
+}
+else if(a>b){
+    console.log("The largest number is"+a)
+}
+else{
+    console.log("they are equal")
+}//leargest of the 2 NUmbers
+
+
+let x=3;
+let y=-7;
+let z=2;
+if (x > 0 && y > 0 && z > 0) {
+    alert("The sign is +");
+  } else if (x < 0 && y < 0 && z < 0) {
+    console.log("The sign is -");
+  } else if (x > 0 && y < 0 && z < 0) {
+    console.log("The sign is +");
+  } else if (x < 0 && y > 0 && z < 0) {
+    console.log("The sign is +");
+  } else {
+    console.log("The sign is -");
+  }//Checking the -sign symbol

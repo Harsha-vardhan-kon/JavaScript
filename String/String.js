00679371fce2f257fb8a9d976f0f9039d842a7f4
Harsha <harshavardhankond@gmail.com>
2025-03-18 16:19:10 +0530

    added my file

--- String/String.js
+++ String/String.js
@@ -7,7 +7,7 @@ console.log(nameOfCar.length)
 let nameOfV="harsha is the one of the human"
 console.log(nameOfV.split(' '));
 
-//Trim (it is used to cut the blank space around the words)
+//Trim (it is used to cut the blank space begining the words)
 let nameOfH="    harsha   is   a   great  human   "
 console.log(nameOfH.trim( ));
 
@@ -21,5 +21,8 @@ console.log(nameOfS.toUpperCase())
 
 //lowercase (it is used convert the data into lowerCase)
 let nameOfG="HARSHA VARDHAN"
-
 console.log(nameOfG.toLowerCase())
+
+//replace (it is used to replace the words)
+let nameOfL="harshavardhan"
+console.log(nameOfL.replace("harshavardhan","konduru"))//first we need to write the old value then camma new value

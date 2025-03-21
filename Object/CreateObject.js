421e596ff0cb5df3ddc5e03ac7b7e681ac7fe77b
Harsha <harshavardhankond@gmail.com>
2025-03-21 11:56:40 +0530

    added new folder

---
+++ Object/CreateObject.js
@@ -0,0 +1,30 @@
+//we can create objects in 3 ways
+//1.object literal ,2.using new keyword,3.using constructor
+
+ const arr=({
+       name:'harsha',
+       age:22,
+       address:"andhraPardesh"
+})
+arr.name='vardhan'
+console.log(arr.name)
+console.log(arr.age)
+
+
+const har=new Object({  //using new keyword
+    name:'babu',
+    age:30
+})
+har.age=40;
+console.log(har.name) 
+console.log(har.age)
+
+//keys,values,
+const valu=({
+    id:1,
+    name:'harsha'
+})
+console.log(Object.keys(valu))//accesing keys shows only id,name in array format
+console.log(Object.values(valu))//accessing values shoes only id value (1) id name(harsha) in array format
+console.log(Object.entries(valu))//accssing entries 
+    
---
+++ TASKS/LibrarySystem.js
@@ -0,0 +1,42 @@
+let library = [
+    {
+        title: 'The Lord of The Rings',
+        author: 'J.R.R. Tolkien',
+        year: 1950
+    },
+    {
+        title: 'The Broken Earth',
+        author: 'N.K. Jemisin',
+        year: 1970
+    },
+    {
+        title: 'Solo Leveling',
+        author: 'Chugong',
+        year: 2020
+    }
+];
+
+
+function getAvailableBooks() {
+    console.log(library.map(Books=>Books.title))
+}
+getAvailableBooks(); 
+
+function searchBook(title){
+    if(title==='Solo Leveling'){
+        console.log(Object.entries(library[2])) 
+    }
+    else if(title==='The Lord of The Rings'){
+        console.log(Object.entries(library[0]))
+    }
+    else if(title==='The Broken Earth'){
+        console.log(Object.entries(library[1]))
+    }
+    else{
+        console.log('book not found')
+    }
+}
+
+
+searchBook('The Lord of The Rings')
+
